23e8e398041afc93af730baf9b046f0651fe094b
diovps <nrharris@unca.edu>
2012-12-06 15:20:31 -0500

    Split seems to be working alright

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ exam3d: exam3d.cpp
 exam5: exam5.cpp
 	$(GCC) $(CLFAGS) -o exam5 exam5.cpp
 
+clean:
+	rm -f exam5
+	rm -f exam3d
--- exam5.cpp
+++ exam5.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(std::string line){
+	
+	std::vector<std::string> words;
+	for(int i = 0; i < line.size(); i++){
+		int index = line.find(' ',i);
+		int rindex = line.rfind(' ');
+		
+		words.push_back(line.substr(i,index-i));
+		i = index;
+		
+		if(index == rindex){
+			words.push_back(line.substr
+				(rindex+1,rindex-line.size()));
+			break;
+		}
+	}
+	return words;
+}
 
 int main(){
 
+	std::string line = "Hello my baby hello my darling";
+	std::vector<std::string> words = split(line);
+	
+	for(int i = 0; i < words.size(); i++){
+		std::cout << words[i] << std::endl;
+	}
 	return 0;
 }
